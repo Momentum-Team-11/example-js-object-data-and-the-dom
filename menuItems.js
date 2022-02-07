840f0bd9b3d12937e482780084f61f569987794d
Amy Gori <amydgori@gmail.com>
2022-02-07 16:20:41 -0500

    Add example code for working with JS objects + the DOM

---
+++ menuItems.js
@@ -0,0 +1,31 @@
+/* DATA */
+const menuItems = [
+  {
+    title: 'Noodles',
+    image: {
+      url: 'https://images.unsplash.com/photo-1569718212165-3a8278d5f624?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      altText: 'bowl of noodles',
+    },
+  },
+  {
+    title: 'Burgers',
+    image: {
+      url: 'https://images.unsplash.com/photo-1550950158-d0d960dff51b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80',
+      altText: 'a hamburger on a bun',
+    },
+  },
+  {
+    title: 'Breakfast',
+    image: {
+      url: 'https://images.unsplash.com/photo-1459789034005-ba29c5783491?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1450&q=80',
+      altText: 'a waffle with yogurt and fruit on top',
+    },
+  },
+  {
+    title: 'Dessert',
+    image: {
+      url: 'https://images.unsplash.com/photo-1514849302-984523450cf4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80',
+      altText: 'a decadent chocolate dessert in a stem glass',
+    },
+  },
+]
